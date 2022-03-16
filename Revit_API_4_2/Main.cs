2388f546870708237cb471510936e87dc792e91c
cadman77 <cadman77@yandex.ru>
2022-03-16 23:53:57 +0300

    Add project files.

---
+++ Revit_API_4_2/Main.cs
@@ -0,0 +1,89 @@
+﻿//Для всех труб выведите в Excel-таблицу следующие значения:
+//имя типа, наружный диаметр, внутренний диаметр, длина.
+
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Revit_API_4_2
+{
+    [Transaction(TransactionMode.Manual)]
+
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string fileName = "PipeInfo_(Revit-API_Lab4-2).XLSX";
+
+            try
+            {
+
+            var pipes = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_PipeCurves)
+                .WhereElementIsNotElementType()
+                .Cast<Pipe>()
+                .ToList();
+
+            string excelPath = Path.Combine(desktopPath, fileName);
+
+            using (FileStream stream = new FileStream(excelPath, FileMode.Create, FileAccess.Write))
+            {
+                IWorkbook workbook = new XSSFWorkbook();
+                ISheet sheet = workbook.CreateSheet("Трубы");
+
+                int rowIndex = 0;
+
+                //"Имя типа;Наружный диаметр, мм;Внутренний диаметр, мм;Длина, мм"
+
+                foreach (var pipe in pipes)
+                {
+
+                    //Parameter type = elem.LookupParameter("Type");
+                    //Parameter outDia = elem.LookupParameter("Outside Diameter");
+                    //Parameter inDia = elem.LookupParameter("Inside Diameter");
+                    //Parameter length = elem.LookupParameter("Length");
+
+                    sheet.SetCellValue(rowIndex, columnIndex: 0, pipe.PipeType.Name);
+                    double outDiaFeet = pipe.get_Parameter(BuiltInParameter.RBS_PIPE_OUTER_DIAMETER).AsDouble();
+                    sheet.SetCellValue(rowIndex, columnIndex: 1, UnitUtils.ConvertFromInternalUnits(outDiaFeet, UnitTypeId.Millimeters));
+                    double inDiaFeet = pipe.get_Parameter(BuiltInParameter.RBS_PIPE_INNER_DIAM_PARAM).AsDouble();
+                    sheet.SetCellValue(rowIndex, columnIndex: 2, UnitUtils.ConvertFromInternalUnits(inDiaFeet, UnitTypeId.Millimeters));
+                    double pipeLength = pipe.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH).AsDouble();
+                    sheet.SetCellValue(rowIndex, columnIndex: 3, UnitUtils.ConvertFromInternalUnits(pipeLength, UnitTypeId.Millimeters));
+                    rowIndex++;
+                }
+
+                workbook.Write(stream);
+                workbook.Close();
+            }
+
+            System.Diagnostics.Process.Start(excelPath);
+
+            TaskDialog.Show("Выполнено", $"Данные о всех ({pipes.Count}шт.) трубах проекта {doc.Title}.RVT выгружены в файл {fileName}{Environment.NewLine}(см. Рабочий стол)");
+
+            }
+            catch
+            {
+                TaskDialog.Show("Ошибка", "При выполнении команды возник сбой");
+                return Result.Failed;
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ Revit_API_4_2/SheetExts.cs
@@ -0,0 +1,30 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.SS.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revit_API_4_2
+{
+    public static class SheetExts
+    {
+        public static void SetCellValue<T>(this ISheet sheet, int rowIndex, int columnIndex, T value)
+        {
+            CellReference cellReference = new CellReference(rowIndex, columnIndex); //var?CellReference
+            IRow row = sheet.GetRow(cellReference.Row); //var?IRow
+            if (row == null)
+                row = sheet.CreateRow(cellReference.Row);
+            ICell cell = row.GetCell(cellReference.Col); //var?ICell 
+            if (cell == null)
+                cell = row.CreateCell(cellReference.Col);
+            if (value is string)
+                cell.SetCellValue((string)(object)value);
+            else if (value is double)
+                cell.SetCellValue((double)(object)value);
+            else if (value is int)
+                cell.SetCellValue((int)(object)value);
+        }
+    }
+}
